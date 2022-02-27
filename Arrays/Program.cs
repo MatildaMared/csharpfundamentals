a0550e2cfa5519df1036f799aa552db3bd52f119
Matilda Mared <matildamared@live.se>
2022-02-27 09:25:57 +0100

    practice with arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿var numbers = new int[] {3, 7, 9, 2, 14, 6};
+
+// Length
+Console.WriteLine("Length: " + numbers.Length);
+
+// IndexOf()
+var index = Array.IndexOf(numbers, 9);
+Console.WriteLine("Index of 9 is: " + index);
+
+// Clear
+Array.Clear(numbers, 0, 2);
+
+Console.WriteLine("\nEffect of clear:");
+foreach (var number in numbers) {
+	Console.Write(number + ", ");
+}
+
+// Copy()
+Console.WriteLine("\n\nCopy of numbers:");
+int[] another = new int[3];
+Array.Copy(numbers, another, 3);
+
+foreach (var number in another) {
+	Console.Write(number + ", ");
+}
+
+// Sort()
+Array.Sort(numbers);
+Console.WriteLine("\n\nSorted numbers:");
+foreach (var number in numbers) {
+	Console.Write(number + ", ");
+}
+
+// Reverse()
+Array.Reverse(numbers);
+Console.WriteLine("\n\nReversed numbers:");
+foreach (var number in numbers) {
+	Console.Write(number + ", ");
+}
