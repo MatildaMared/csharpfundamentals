798d507ba398e5cef49841b7131ee0564da44eed
Matilda Mared <matildamared@live.se>
2022-03-01 07:15:04 +0100

    practice with files

---
+++ Files/Program.cs
@@ -0,0 +1,19 @@
+﻿// Working with Files and directories
+
+var path = @"/Users/matildamared/Documents/programming/c#/CSharpFundamentals/Files/test.txt";
+
+File.Create(path);
+
+var fileInfo = new FileInfo(path);
+
+if (fileInfo.Exists) {
+	Console.WriteLine("This file exists!");
+}
+
+var pathToProject = @"/Users/matildamared/Documents/programming/c#/CSharpFundamentals";
+
+var directories = Directory.GetDirectories(pathToProject, "*.*", SearchOption.AllDirectories);
+
+foreach (var directory in directories) {
+	Console.WriteLine(directory);
+}
